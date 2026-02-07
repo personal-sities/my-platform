91db795c7254f22badf333ba90ce41544266e7d7
personal-sities <umirbekovdide@gmail.com>
2026-02-07 19:13:23 +0500

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,32 @@
+if (!localStorage.getItem("user")) {
+  localStorage.setItem("user", JSON.stringify({
+    login: "admin",
+    password: "1234"
+  }));
+}
+
+function login() {
+  const l = login.value;
+  const p = password.value;
+  const u = JSON.parse(localStorage.getItem("user"));
+
+  if (l === u.login && p === u.password) {
+    location.href = "dashboard.html";
+  } else {
+    alert("Login yoki parol noto‘g‘ri");
+  }
+}
+
+function resetData() {
+  const u = JSON.parse(localStorage.getItem("user"));
+
+  if (oldLogin.value === u.login && oldPass.value === u.password) {
+    u.login = newLogin.value  u.login;
+    u.password = newPass.value  u.password;
+    localStorage.setItem("user", JSON.stringify(u));
+    alert("Yangilandi");
+    location.href = "index.html";
+  } else {
+    alert("Eski ma’lumotlar noto‘g‘ri");
+  }
+}
