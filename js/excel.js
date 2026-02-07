bde2403dc5a1a24fdde03776f84db0037be9f824
personal-sities <umirbekovdide@gmail.com>
2026-02-07 19:14:03 +0500

    Create excel.js

---
+++ js/excel.js
@@ -0,0 +1,6 @@
+function exportExcel(data, name) {
+  const ws = XLSX.utils.json_to_sheet(data);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Data");
+  XLSX.writeFile(wb, name);
+}
