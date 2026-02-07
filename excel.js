cd1170e69857ce4a754e322d1af7d14f836eaed7
personal-sities <umirbekovdide@gmail.com>
2026-02-07 19:11:26 +0500

    Delete excel.js

--- excel.js
+++
@@ -1,6 +0,0 @@
-function exportExcel(data, name) {
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "Data");
-  XLSX.writeFile(wb, name);
-}
