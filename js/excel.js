2c74af797269c0a6cc02dc85fec4fbebf834f95c
personal-sities <umirbekovdide@gmail.com>
2026-02-11 19:34:37 +0500

    Update excel.js

--- js/excel.js
+++ js/excel.js
@@ -1,6 +1,38 @@
-function exportExcel(data, name) {
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "Data");
-  XLSX.writeFile(wb, name);
-}
+const SCRIPT_URL = "SIZNING_WEB_APP_URL_BU_YERGA";
+
+document.querySelectorAll(".sheet-form").forEach(form => {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const formData = {};
+    const inputs = form.querySelectorAll("input, select, textarea");
+
+    inputs.forEach(input => {
+      if (input.name) {
+        formData[input.name] = input.value;
+      }
+    });
+
+    const data = {
+      page: form.dataset.page,
+      formData: formData
+    };
+
+    fetch(SCRIPT_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(response => {
+      alert("Ma'lumot saqlandi!");
+      form.reset();
+    })
+    .catch(error => {
+      alert("Xatolik yuz berdi!");
+      console.error(error);
+    });
+  });
+});
